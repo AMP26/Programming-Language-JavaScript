b548e94b06b2fc9119c76720d42ec917eb6f00de
AMP26 <amit.patnaik0126@gmail.com>
2025-06-19 09:05:11 +0530

    JavaScript Loops

---
+++ 04 - Loops/01-loops.js
@@ -0,0 +1,20 @@
+// for loop
+console.log("Printing odd numbers using \"for\" loop");
+for(let i = 1; i <= 10; i += 2)
+  console.log(i);
+
+// while loop
+console.log("Printing even numbers using \"while\" loop");
+let j = 0;
+while(j <= 10) {
+  console.log(j);
+  j += 2;
+}
+
+// do while loop
+console.log("Printing 1 to 10 numbers using \"do while\" loop");
+let k = 1;
+do {
+console.log(k);
+k++;
+} while(k <= 10);
