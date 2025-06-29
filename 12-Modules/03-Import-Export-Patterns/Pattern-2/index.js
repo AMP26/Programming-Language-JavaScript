0294764ee223566287a260cdd9eca5a7c2737d7b
AMP26 <amit.patnaik0126@gmail.com>
2025-06-29 20:23:56 +0530

    Added files in Modules

---
+++ 12-Modules/03-Import-Export-Patterns/Pattern-2/index.js
@@ -0,0 +1,4 @@
+const func = require("./pattern2");
+
+const sum = func(4, 5);
+console.log(sum);
---
+++ 12-Modules/03-Import-Export-Patterns/Pattern-4/pattern4.js
@@ -0,0 +1,10 @@
+// Pattern  - 4
+// Summation
+module.exports.add = function(x, y) {
+  return x + y;
+}
+
+// Substract
+module.exports.minus = function(x, y) {
+  return y - x;
+}
